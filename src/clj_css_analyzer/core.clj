3d8315e9f99e9282cd15fcdd55c6eab174a42e31
Andrew Sernyak <ndrew.sernyak@gmail.com>
2014-07-17 00:48:36 +0300

    unified results of css and html parsing

--- src/clj_css_analyzer/core.clj
+++ src/clj_css_analyzer/core.clj
@@ -8,7 +8,7 @@
 ;; inputs
 ;;
 
-(def test-css (slurp "sample.css"))
+(def test-css (slurp "sample1.css"))
 (def test-html (slurp "sample.html"))
 
 ;;
@@ -17,19 +17,50 @@
 
 ;; css - vector of :*.selector [:*.selector-after-comma] [:sub-selector-with-no-asterisk] {css-body}, [...], ...
 (def css-data (css/parse-css test-css))
-
-; how to format extracted classes from html below?
-(css/property-as-hash-map "baz buzz.bizz")
-
-
 ;; html
 (def html-data (l/parse test-html))
 
 (def styled-els (l/select html-data (l/attr? :class)))
 
+::
 ;; aggregate n analyze
-(def all-styles (reduce (fn [res node]
-                            (assoc res (:class (:attrs node)) node))
-                        {} styled-els))
+::
 
+; css
+(defn- parsed-css-to-vectors[css-data]
+  (cons (vec (filter #(not (vector? %)) css-data))
+       (filter vector? css-data)))
+
+(defn- css-map[css-data]
+  (reduce
+    #(let [v (peek %2)
+        k (pop %2)]
+      (assoc %1 k v))
+      {} (parsed-css-to-vectors css-data)))
+
+
+; html
+(defn normalize-class-selector [class-clause]
+  (-> class-clause
+    (clojure.string/trim)
+    (clojure.string/split #"\s+")
+    (->> (map #(keyword (str "*." %))))
+      vec))
+
+(defn- css-from-html-map[elements]
+  (reduce (fn [res node]
+                            (assoc res
+                              (normalize-class-selector (:class (:attrs node)))
+                              {} ;node ; instead of actual content
+                              ))
+                        {} elements))
+
+
+;; results
+
+(def styles-from-css (css-map css-data))
+(def styles-from-html (css-from-html-map styled-els))
+
+;(pprint (keys styles-from-css))
+;(pprint (keys styles-from-html))
 
